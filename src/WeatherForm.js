4fa333e190ee36fb59981b98831dbd83122041b0
robertagelo <123727348+robertagelo@users.noreply.github.com>
2023-08-13 13:05:22 +0200

    Added basic weather app

---
+++ src/WeatherForm.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function WeatherForm() {
+  let [city, setCity] = useState("");
+  let [weather, setWeather] = useState("");
+
+  function updateCity(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
+
+  function displayWeather(response) {
+    let temperature = Math.round(response.data.temperature.current);
+
+    setWeather(
+      <ul>
+        <li>Temperature: {temperature}°C</li>
+        <li>Description: {response.data.condition.description}</li>
+        <li>Humidity: {response.data.temperature.humidity}%</li>
+        <li>Wind: {response.data.wind.speed} km/h</li>
+        <li>
+          <img src={response.data.condition.icon_url} alt="weather icon" />
+        </li>
+      </ul>
+    );
+  }
+
+  function getWeather(event) {
+    event.preventDefault();
+    const key = "96f59ob69a32facbb34b2tdb5d2e7405";
+    const url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}`;
+    axios.get(url).then(displayWeather);
+  }
+
+  return (
+    <div className="container">
+      <form onSubmit={getWeather}>
+        <input
+          type="text"
+          placeholder="Enter a city..."
+          onChange={updateCity}
+        />
+        <button input type="submit">
+          Search
+        </button>
+      </form>
+      {weather}
+    </div>
+  );
+}
