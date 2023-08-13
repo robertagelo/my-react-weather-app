c1fd1413a6af79fc703022a9254695e845963c05
robertagelo <123727348+robertagelo@users.noreply.github.com>
2023-08-13 15:04:38 +0200

    Added repository link and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import WeatherForm from "./WeatherForm";
 
@@ -6,6 +7,11 @@ function App() {
     <div className="App">
       <h1>Weather app</h1>
       <WeatherForm />
+      <p>
+        This is only the beginning! Entire project is work in progress and it
+        can be found openly sourced{" "}
+        <a href="https://github.com/robertagelo/my-react-weather-app">here</a>
+      </p>
     </div>
   );
 }
